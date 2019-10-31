675e76b64705c141277ec7bea6ee18ff869df651
footman136 <liugang2006@hotmail.com>
2019-10-31 23:03:34 +0800

    添加选中框的修改脚本

---
+++ Assets/Arts/Effect/SelectCircle/SelectCircle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCircle : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        SetSize(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSize(float size)
+    {
+        ParticleSystem.EmitParams param = new ParticleSystem.EmitParams
+        {
+            startSize = size,
+        };
+        ParticleSystem.Particle particle = 
+        _particleSystem.Emit(param,1);
+        transform.localScale = new Vector3(size,0,size);
+    }
+}
